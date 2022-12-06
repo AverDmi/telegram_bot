b2e4b3ffe77f93a5d6b29b0bc19ccf375c6c5df7
Dmitriy Averyanov <dimthomas1@mail.ru>
2022-12-06 11:38:21 +0300

    Add Microservice Node

---
+++ dispatcher/src/main/java/com/dimthomas/service/impl/AnswerConsumerImpl.java
@@ -0,0 +1,24 @@
+package com.dimthomas.service.impl;
+
+import com.dimthomas.controller.UpdateController;
+import com.dimthomas.service.AnswerConsumer;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+import static com.dimthomas.model.RabbitQueue.ANSWER_MESSAGE;
+
+@Service
+public class AnswerConsumerImpl implements AnswerConsumer {
+    private final UpdateController updateController;
+
+    public AnswerConsumerImpl(UpdateController updateController) {
+        this.updateController = updateController;
+    }
+
+    @Override
+    @RabbitListener(queues = ANSWER_MESSAGE)
+    public void consume(SendMessage sendMessage) {
+        updateController.setView(sendMessage);
+    }
+}
